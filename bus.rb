a6c631ed6a28b8a9ebd77898db04fd819612084b
David Salgado <david@digitalronin.com>
2017-01-15 16:02:19 +0000

    move all api code to lookup.rb

--- bus.rb
+++ bus.rb
@@ -11,11 +11,6 @@ require './lib/lookup'
 set :views,      File.dirname(__FILE__) + '/views'
 set :public_dir, File.dirname(__FILE__) + '/static'
 
-# The part before the docpath is in upper case because
-# that results in a smaller (i.e. fewer pixels) QR code
-# which is easier to scan
-STOP_URL = 'HTTP://BUS.ABSCOND.ORG/stop'
-
 get '/' do
   if params[:search] && params[:search] != ""
     if id = Lookup.id_from_sms_code(params[:search])
@@ -30,12 +25,8 @@ get '/' do
   erb :index
 end
 
-get '/nearby' do
-  erb :nearby
-end
-
 get '/stop/:stop_id' do |stop_id|
-  @arrivals = get_arrivals(stop_id)
+  @arrivals = Lookup.get_arrivals(stop_id)
   if @arrivals.any?
     arr = @arrivals.first
     @stop_name = "#{arr["stationName"]} (#{arr["platformName"]})"
@@ -44,20 +35,6 @@ get '/stop/:stop_id' do |stop_id|
 end
 
 get '/stop/:stop_id/partial' do |stop_id|
-  @arrivals = get_arrivals(stop_id)
+  @arrivals = Lookup.get_arrivals(stop_id)
   erb :indicator_table, layout: false
-end
-
-def get_arrivals(stop_id)
-  response = make_request(stop_id)
-  JSON.parse(response).sort {|a,b| a["timeToStation"] <=> b["timeToStation"]}
-end
-
-def get_stop_json(stop_id)
-  response = make_request(stop_id)
-  @json = JSON.parse(response)
-end
-
-def make_request(stop_id)
-  Net::HTTP.get(URI.parse("https://api.tfl.gov.uk/StopPoint/#{stop_id}/arrivals"))
 end
--- lib/lookup.rb
+++ lib/lookup.rb
@@ -1,22 +1,27 @@
 class Lookup
 
-  def self.id_from_sms_code(value)
-    new.id_from_sms_code value
-  end
-
   # Take a 5-digit bus stop SMS code (e.g. '72269') and return the corresponding Naptan ID (490008041N)
-  def id_from_sms_code(value)
+  def self.id_from_sms_code(value)
     puts "Lookup#find(#{value}) ..............................."
-    response = get("https://api.tfl.gov.uk/StopPoint/Sms/#{value}")
+    response = self.get("https://api.tfl.gov.uk/StopPoint/Sms/#{value}")
     # Although the API docs say we should get JSON back, and that's what happens on the TFL API explorer,
     # https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/StopPoint/StopPoint_GetBySms
     # in practice, I only ever got redirected to /StopPoint/[naptan ID]
     (response['location']) ? response['location'].sub(/.*\//, '') : nil
   end
 
-  private
+  def self.get_arrivals(stop_id)
+    response = make_request(stop_id)
+    JSON.parse(response).sort {|a,b| a["timeToStation"] <=> b["timeToStation"]}
+  end
 
-  def get(url)
+  # private
+
+  def self.make_request(stop_id)
+    Net::HTTP.get(URI.parse("https://api.tfl.gov.uk/StopPoint/#{stop_id}/arrivals"))
+  end
+
+  def self.get(url)
     uri = URI(url)
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
